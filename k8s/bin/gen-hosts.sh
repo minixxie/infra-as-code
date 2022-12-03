8ef4dd90404c95f7d2a905b6d8c2644f9ba02543
Simon Xie <simon.xie@kuulabu.com>
2022-12-03 19:38:00 +0800

    Added hosts for argocd

--- k8s/bin/gen-hosts.sh
+++ k8s/bin/gen-hosts.sh
@@ -3,11 +3,12 @@
 cat <<EOF
 ## Copy and paste the following 9 lines into your /etc/hosts file:
 $1  k8s.local
+$1  argo-cd.local
+$1  kubeapps.local
 $1  grafana.local
 $1  prometheus.local
 $1  mysql-exporter.local
 $1  mimir.local
-$1  kubeapps.local
 $1  kafka.local
 $1  kafka-ui.local
 $1  harbor.local
