76c204c6d2f508a3b9cc2bfdc41c670a9b62b891
Simon Xie <simon.xie@kuulabu.com>
2022-12-10 09:56:39 +0800

    Added hosts

--- k8s/bin/gen-hosts.sh
+++ k8s/bin/gen-hosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat <<EOF
-## Copy and paste the following 9 lines into your /etc/hosts file:
+## Copy and paste the following 12 lines into your /etc/hosts file:
 $1  k8s.local
 $1  argo-cd.local
 $1  kubeapps.local
@@ -11,6 +11,8 @@ $1  mysql-exporter.local
 $1  mimir.local
 $1  kafka.local
 $1  kafka-ui.local
+$1  redisinsight.local
+$1  pgadmin4.local
 $1  harbor.local
 $1  echo-server-api.local
 # if you install local-stack in k8s:
